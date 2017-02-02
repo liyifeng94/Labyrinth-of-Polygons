e86c910ee2180620b71bb13d5105ef68a6aacfb9
Ethan Li <yf8li@uwaterloo.ca>
2017-02-02 17:10:49 -0500

    Created a basic game state classes.

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[Serializable]
+public class GameState
+{
+    public int Gems;
+    public List<int> TowerUnlockList;
+}
---
+++ Assets/Scripts/LevelState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SocialPlatforms.Impl;
+
+[Serializable]
+public class LevelState
+{
+    public int Score;
+    public int Gems;
+    public int Gold;
+    public int Level;
+
+    //TODO: save grid state
+}
