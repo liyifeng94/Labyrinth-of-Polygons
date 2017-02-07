6082f6fd507b521f0f4af6bfb3ba670fe9ec9b92
Yishi Wang <y785wang@uwaterloo.ca>
2017-02-07 18:47:00 -0500

    Tower

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Tower : MonoBehaviour {
 
@@ -8,22 +7,66 @@ public class Tower : MonoBehaviour {
 	public uint AttackRange { get; private set; }
 
 
-	public uint hitPoint;
-	public uint attackDamage;
-	public uint attackSpeed;  // trsf only
-	//private uint targetEnemy;
-	//private Manager manager; // class name issue
-	//private Block[] block;   // class name issue
-	//private uint bIndex;
-	//private Enemy target;    // class name issue
+	public uint HitPoint;
+	public uint AttackDamage;
+	public uint AttackSpeed;  // trsf only
+	//private uint TargetEnemy;
+	//private Manager Nanager; // class name issue
+	//private Block[] Block;   // class name issue
+	private uint BIndex;
+	//private Enemy Target;    // class name issue
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    Build();
 	}
 	
 	// Update is called once per frame
 	void LastUpdate () {
 	
 	}
+
+    public void Build()
+    {
+        //m.build(x, y, r, this);
+    }
+
+    public void Destory()
+    {
+        for (int i = 0; i < BIndex; i++)
+        {
+            //block[i].deLink(this);
+        }
+    }
+    /*
+    public void SetTarget(Enemy t)
+    {
+        Target = t;
+    }
+
+    public void AttackEnemy(Enemy t)
+    {
+        t.ReceiveAttack(ad);
+    }
+    */
+
+    public void ReceiveAttack(uint ad)
+    {
+        if (HitPoint > ad)
+        {
+            HitPoint -= ad;
+        }
+        else
+        {
+            Destory();
+        }
+    }
+    /*
+    public void assignBlock(Block b)
+    {
+        Block[bIndex] = b;
+        BIndex++;
+    }
+    */
 }
