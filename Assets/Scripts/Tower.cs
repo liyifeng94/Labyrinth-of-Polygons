bef964b898a8029652a3f57960cc4e557bb45f15
Yishi Wang <y785wang@uwaterloo.ca>
2017-02-07 17:19:53 -0500

    Tower

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+
+	public uint X { get; private set; }
+	public uint Y { get; private set; }
+	public uint AttackRange { get; private set; }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LastUpdate () {
+	
+	}
+}
