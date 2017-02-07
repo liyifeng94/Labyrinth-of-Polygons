daed678f050f295e9ef81a7ad00af7b69e7d8555
Yishi Wang <y785wang@uwaterloo.ca>
2017-02-07 17:57:59 -0500

    tower

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,14 +6,16 @@ public class Tower : MonoBehaviour {
 	public uint X { get; private set; }
 	public uint Y { get; private set; }
 	public uint AttackRange { get; private set; }
-	private uint hitPoint;
-	private uint attackDamage;
-	private uint attackSpeed;  // trsf only
-	private uint targetEnemy;
-	private Manager manager; // class name issue
-	private Block[] block;   // class name issue
-	private uint bIndex;
-	private Enemy target;    // class name issue
+
+
+	public uint hitPoint;
+	public uint attackDamage;
+	public uint attackSpeed;  // trsf only
+	//private uint targetEnemy;
+	//private Manager manager; // class name issue
+	//private Block[] block;   // class name issue
+	//private uint bIndex;
+	//private Enemy target;    // class name issue
 
 	// Use this for initialization
 	void Start () {
