6f5666932a99dbe54d911859e51322d57fbfd872
Yishi Wang <y785wang@uwaterloo.ca>
2017-02-09 20:20:21 -0500

    build, destory and attacck timer are done

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,55 +1,65 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Tower : MonoBehaviour {
 
 	public uint X { get; private set; }
 	public uint Y { get; private set; }
 	public uint AttackRange { get; private set; }
-
-
 	public uint HitPoint;
 	public uint AttackDamage;
 	public uint AttackSpeed;  // trsf only
-	//private uint TargetEnemy;
-	//private Manager Nanager; // class name issue
-	//private Block[] Block;   // class name issue
-	private uint BIndex;
-	//private Enemy Target;    // class name issue
 
-	// Use this for initialization
-	void Start ()
+    private HashSet<Enemy> _enemies = new HashSet<Enemy>();
+    private GameBoard _gameBoard;
+    private float _loadingTime;
+
+    void Start ()
 	{
 	    Build();
+	    _enemies = null;
+	    _gameBoard = GameManager.Instance.CurrentLevelManager.GameBoardSystem;
+        _loadingTime = AttackSpeed;
 	}
-	
-	// Update is called once per frame
+
 	void LastUpdate () {
-	
+	    if (null != _enemies)
+	    {
+	        if (_loadingTime >= AttackSpeed)
+	        {
+	            AttackEnemy(_enemies.First()); // make this simple, just attack the first one
+	            _loadingTime = 0.0f;
+                _enemies.Clear();
+	        }
+	        else
+	        {
+                _loadingTime += Time.deltaTime;
+	        }
+        }
 	}
 
     public void Build()
     {
-        //m.build(x, y, r, this);
+
+        _gameBoard.BuildTower(this);
     }
 
     public void Destory()
     {
-        for (int i = 0; i < BIndex; i++)
-        {
-            //block[i].deLink(this);
-        }
+        _gameBoard.RemoveTower(this);
     }
-    /*
-    public void SetTarget(Enemy t)
+    
+    public void AddEnemy(Enemy t)
     {
-        Target = t;
+        _enemies.Add(t);
     }
 
     public void AttackEnemy(Enemy t)
     {
-        t.ReceiveAttack(ad);
+        t.damaged(AttackDamage);
     }
-    */
+    
 
     public void ReceiveAttack(uint ad)
     {
@@ -62,11 +72,4 @@ public class Tower : MonoBehaviour {
             Destory();
         }
     }
-    /*
-    public void assignBlock(Block b)
-    {
-        Block[bIndex] = b;
-        BIndex++;
-    }
-    */
 }
