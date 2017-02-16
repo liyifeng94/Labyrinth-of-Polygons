ddd0e3f5ffbc02e490ea1f59779eebc6998ae20d
Yishi Wang <y785wang@uwaterloo.ca>
2017-02-16 17:04:20 -0500

    123

---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TowerController : MonoBehaviour
+{
+    public static TowerController Instance;
+    public List<GameObject> Towers;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public bool BuildTower(uint x, uint y, uint index)
+    {
+        Vector3 towerPosition;
+        towerPosition.x = x;
+        towerPosition.y = y;
+        towerPosition.z = 0f;
+        //GameObject towerGameObject = new GameObject();
+        //Tower towerPtr = towerGameObject.GetComponent<Tower>();
+        //towerGameObject = Instantiate(towerPtr, towerPosition, Quaternion.identity) as GameObject;
+        return true;
+    }
+}
