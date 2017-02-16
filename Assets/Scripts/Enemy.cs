f2649181af7cf1b1199781f9ed1f71cd67b37de0
Tianchen <tianchen.bi@gmail.com>
2017-02-16 17:18:23 -0500

    added some enemy controller implementation added enemy image

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,17 +11,25 @@ public class Enemy : MonoBehaviour
     public uint GridY { get; private set; }
 
     private uint _x;
-    private uint _y = 0;
+    private uint _y;
     private uint _speed;
     private uint _attackRange;
     private uint _hp;
+    private uint index;
 
     private List<GridSystem.Cell> _path;
 
     private Direction _dir = Direction.Down;
 
-    public void Spawn()
-    { 
+    public Enemy(uint x, uint y,List<GridSystem.Cell> path)
+    {
+        _x = x;
+        _y = y;
+        _speed = 50;
+        _hp = 1;
+        _path = path;
+        index = 0;
+
     }
 
     public void SetPos(uint xPos, uint yPos) {
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,17 +4,27 @@ using System.Collections.Generic;
 
 public class EnemyController : MonoBehaviour
 {
-
+    public static GameManager Instance;
     private List<Enemy> _enemies;
     private float _spawnTimer = Time.time;
 
 
+    public void SpawnEnemy()
+    {
+        List<GridSystem.Cell> entrances = Instance.CurrentLevelManager.GameBoardSystem.GameGridSystem.MainGameGrid.Entrances;
+        List<GridSystem.Cell> path = Instance.SearchPathFrom(entrances[0].X, entrances[0].Y);
+        Enemy newEnemy = new Enemy(entrances[0].X, entrances[0].Y, path);
+        _enemies.Add(newEnemy);
+
+
+    }
+
 	// Use this for initialization
-	void Start () {
+    public void Start () {
 	
 	}
 
-    bool WaveCleared()
+    public bool WaveCleared()
     {
         return _enemies.Count==0;
     }
@@ -22,7 +32,7 @@ public class EnemyController : MonoBehaviour
 
 
 	// Update is called once per frame
-	void Update () {
-	
+	public void Update () {
+	    
 	}
 }
