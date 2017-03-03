84e5ac700623ca207254e1c939026e990933627f
EthanLi-LAPTOPW\Ethan Li <yf8li@uwaterloo.ca>
2017-03-02 23:26:21 -0500

    Added shuffle function.

---
+++ Assets/Scripts/ListExt.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class ListExt
+{
+    // Using Fisher–Yates shuffle
+    public static void Shuffle<T>(List<T> list)
+    {
+        int last = list.Count - 1;
+        for (int i = 0; i < last; ++i)
+        {
+            int j = UnityEngine.Random.Range(i, list.Count);
+            T temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+    }
+}
