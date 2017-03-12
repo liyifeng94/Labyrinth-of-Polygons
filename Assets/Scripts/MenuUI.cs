95482c664659efa382089a38f3a5261a12c4e917
EthanLi-LAPTOPW\Ethan Li <yf8li@uwaterloo.ca>
2017-03-12 18:32:58 -0400

    Added game version on the game menu.

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour
+{
+    public Text VersionText;
+
+    void Start()
+    {
+        VersionText.text = Application.version;
+    }
+
+    public void LoadLevelByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void LoadLevelByName(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
