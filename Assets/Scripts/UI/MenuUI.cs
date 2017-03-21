4f0af60a32c10e4fd4755fcfe645e8c1038a87e2
Ethan Li <yf8li@uwaterloo.ca>
2017-03-21 17:48:37 -0400

    Added more menu callback functions

--- Assets/Scripts/UI/MenuUI.cs
+++ Assets/Scripts/UI/MenuUI.cs
@@ -1,15 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Xml.Serialization;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuUI : MonoBehaviour
 {
     public Text VersionText;
+    public GameObject MainMenuPanel;
+    public GameObject StartOptionsPanel;
 
     void Start()
     {
         if (VersionText!=null)
             VersionText.text = Application.version;
+    }
+
+    public void BackToMainMenu()
+    {
+        
+    }
+
+    public void SetStartOptions()
+    {
+        
     }
 
     public void LoadLevelByIndex(int index)
