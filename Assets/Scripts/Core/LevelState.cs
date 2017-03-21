101f3a47cb9c438d06e1b371729df2bde734322c
Rick-Xia <danteveqann@gmail.com>
2017-03-21 19:27:57 -0400

    divide

--- Assets/Scripts/Core/LevelState.cs
+++ Assets/Scripts/Core/LevelState.cs
@@ -20,7 +20,7 @@ public class LevelState
         FinalScore = (uint) Score;
         FinalScore *= gameOptions.Width + gameOptions.Height + gameOptions.Entrances;
         FinalScore *= (uint)Level;
-        FinalScore += (uint) Score * gameOptions.Obstacles;
+        FinalScore += (uint) Score * ((160 - gameOptions.Obstacles) / 2);
         FinalScore += (uint)Gold;
         return FinalScore;
     }
