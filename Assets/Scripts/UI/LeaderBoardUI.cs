88a18078c8a3914311ae26e2a8b30e4f4ca8cf3d
Rick-Xia <danteveqann@gmail.com>
2017-03-27 16:32:13 -0400

    finish new feature

--- Assets/Scripts/UI/LeaderBoardUI.cs
+++ Assets/Scripts/UI/LeaderBoardUI.cs
@@ -8,6 +8,8 @@ public class LeaderBoardUI : MonoBehaviour
     public Text NameResultText;
     public Text TimeResultText;
     public Text ScoreResultText;
+    public GameObject ContentPanel;
+    public GameObject ChildPanel;
 
 	// Use this for initialization
 	void Start ()
@@ -15,15 +17,30 @@ public class LeaderBoardUI : MonoBehaviour
 	    NameResultText.text = "";
 	    TimeResultText.text = "";
 	    ScoreResultText.text = "";
-
-	    HighScoreBoard temp = GameManager.Instance.LocalHighScoreBoard;
+        
+        HighScoreBoard temp = GameManager.Instance.LocalHighScoreBoard;
 	    List<HighScoreBoard.HighScoreEntry> highScoreList = temp.ScoreList.MainList;
-	    foreach (var entry in highScoreList)
+	    for(int i = 0; i < highScoreList.Count; i++)
 	    {
-	        NameResultText.text += entry.Name + "\n";
-	        TimeResultText.text += entry.DateTime + "\n";
-	        ScoreResultText.text += entry.Score.ToString() + "\n";
-	    }
+
+            GameObject Child2 = Instantiate(ChildPanel);
+            Child2.transform.SetParent(ContentPanel.transform, false);
+
+            Text nameresult = Instantiate(NameResultText);
+            nameresult.transform.SetParent(Child2.transform, false);
+            nameresult.fontSize = 1;
+            nameresult.GetComponent<Text>().text = highScoreList[i].Name;
+
+            Text timeresult = Instantiate(TimeResultText) as Text;
+            timeresult.transform.SetParent(Child2.transform, false);
+            timeresult.fontSize = 1;
+            timeresult.GetComponent<Text>().text = highScoreList[i].DateTime;
+
+            Text scoreresult = Instantiate(TimeResultText) as Text;
+            scoreresult.transform.SetParent(Child2.transform, false);
+            scoreresult.fontSize = 1;
+            scoreresult.GetComponent<Text>().text = highScoreList[i].Score.ToString();
+        }
 	}
 	
 	// Update is called once per frame
